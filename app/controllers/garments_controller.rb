c9197976a11320ac30e93d23a6ce8d3d73c109c3
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-25 12:50:25 -0700

    updates controllers for Garment

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,5 +1,8 @@
 class GarmentsController < ApplicationController
+before_action :set_garment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @garments = Garment.all
   end
 
   def show
@@ -9,20 +12,38 @@ class GarmentsController < ApplicationController
   end
 
   def new
+    @garment = Garment.new
   end
 
   def create
+    @garment = Garment.new(garment_params)
 
+    if @garment.save
+      redirect_to garments_path
+    else
+      render :new
+    end
   end
 
   def update
-
+    if @garment.update_attributes(garment_params)
+      redirect_to @garment
+    else
+      render: edit
+    end
   end
 
   def destroy
-
+    @garment.destroy
+    redirect_to garments_path
   end
 
 private
+  def set_garment
+    @garment = Garment.find(params[:id])
+  end
 
+  def garment_params
+    params.require(:garment).permit(:name, :color, :material, :price, :waterproof)
+  end
 end
