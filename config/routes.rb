b5a3ce3026fbb46582c748f5ccabcb7c7a80e466
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-25 12:37:15 -0700

    adds view templates for garments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'garments/index'
+
+  get 'garments/show'
+
+  get 'garments/edit'
+
+  get 'garments/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
